5c24b8a4a63ead1f20b9c08416c91564dfc93605
Mateusz Okulewicz <mat@citrusad.com>
2019-03-23 17:13:09 +0100

    styles for favourites mark

--- frontend/src/app/shared/animations.ts
+++ frontend/src/app/shared/animations.ts
@@ -1,9 +1,4 @@
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-} from '@angular/animations';
+import {trigger, transition, style, animate} from '@angular/animations';
 
 export const slideInOutLeft = trigger('slideInOutLeft', [
   transition(':enter', [
@@ -29,4 +24,38 @@ export const slideInOutRight = trigger('slideInOutRight', [
       style({opacity: 0, transform: 'translateX(100%)'}),
     ),
   ]),
+]);
+
+export const slideInOutTop = trigger('slideInOutTop', [
+  transition(':enter', [
+    style({opacity: 0, transform: 'translateY(100%)'}),
+    animate('300ms ease-in', style({opacity: 1, transform: 'translateY(0%)'})),
+  ]),
+  transition(':leave', [
+    animate(
+      '300ms ease-in',
+      style({opacity: 0, transform: 'translateY(100%)'}),
+    ),
+  ]),
+]);
+
+export const slideInOutBottom = trigger('slideInOutBottom', [
+  transition(':enter', [
+    style({opacity: 0, transform: 'translateY(-100%)'}),
+    animate('300ms ease-in', style({opacity: 1, transform: 'translateY(0%)'})),
+  ]),
+  transition(':leave', [
+    animate(
+      '300ms ease-in',
+      style({opacity: 0, transform: 'translateY(-100%)'}),
+    ),
+  ]),
+]);
+
+export const fadeIn = trigger('fadeIn', [
+  transition(':enter', [
+    style({opacity: 0}),
+    animate('300ms ease-in', style({opacity: 1})),
+  ]),
+  transition(':leave', [animate('300ms ease-in', style({opacity: 0}))]),
 ]);
--- frontend/src/app/shared/elements/favourites-mark/favourites-mark.component.ts
+++ frontend/src/app/shared/elements/favourites-mark/favourites-mark.component.ts
@@ -1,8 +1,11 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {slideInOutTop, slideInOutBottom, fadeIn} from '../../animations';
 
 @Component({
   selector: 'd20md-favourites-mark',
   templateUrl: './favourites-mark.component.html',
+  styleUrls: ['./favourites-mark.component.scss'],
+  animations: [slideInOutTop, slideInOutBottom],
 })
 export class FavouriteMarkComponent {
   @Input()

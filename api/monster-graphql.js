ecac428c98de38cab3791ca5dc374201c57f51a1
Mateusz Okulewicz <mat@citrusad.com>
2019-04-02 09:19:16 +0200

    get user id from context

--- api/monster-graphql.js
+++ api/monster-graphql.js
@@ -87,7 +87,7 @@ const monsterType = new GraphQLObjectType({
     isInFavourites: {
       type: GraphQLBoolean,
       resolve: (obj, args, context) => {
-        const userId = _.get(context, 'body.variables.userId');
+        const userId = _.get(context, 'body.variables.userId') || _.get(context, 'user.id');
         if (!userId) {
           return null;
         }

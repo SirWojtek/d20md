556c7038c83f923d114d79a38017ccd198e2a275
Mateusz Okulewicz <mat@citrusad.com>
2019-04-11 14:53:59 +0200

    e2e for favourites on show feat page

--- frontend/e2e/scenarios/favourites-show.spec.ts
+++ frontend/e2e/scenarios/favourites-show.spec.ts
@@ -8,6 +8,8 @@ import {FindMonsterPage} from '../pages/find-monster.page';
 import {IFavouritesInfo} from '../pages/elements/favourites/favourites.elements';
 import {FindSpellPage} from '../pages/find-spell.page';
 import {ShowSpellPage} from '../pages/show-spell.page';
+import {FindFeatPage} from '../pages/find-feat.page';
+import {ShowFeatPage} from '../pages/show-feat.page';
 
 describe('favourites show', () => {
   let framePage: FramePage;
@@ -93,7 +95,7 @@ describe('favourites show', () => {
     });
   });
 
-  fdescribe('spell', () => {
+  describe('spell', () => {
     let findSpellPage: FindSpellPage;
     let showSpellPage: ShowSpellPage;
 
@@ -161,6 +163,75 @@ describe('favourites show', () => {
       await framePage.navigateToFavourites();
       isSpellInFavourites = await favouritesPage.isSpellPresent(testSpellName);
       expect(isSpellInFavourites).toBeFalsy();
+    });
+  });
+
+  describe('feat', () => {
+    let findFeatPage: FindFeatPage;
+    let showFeatPage: ShowFeatPage;
+
+    let testFeatName;
+
+    beforeEach(() => {
+      findFeatPage = new FindFeatPage();
+      showFeatPage = new ShowFeatPage();
+
+      testFeatName = 'Power Attack';
+    });
+
+    beforeEach(async () => {
+      await findFeatPage.navigateTo();
+    });
+
+    async function assertFavouriteOnShowPage(
+      added: boolean,
+    ): Promise<IFavouritesInfo> {
+      const favourite = await showFeatPage.getFavouritesInfo();
+      expect(favourite.added).toEqual(added);
+      return favourite;
+    }
+
+    it('should not show favourite marks when not logged in', async () => {
+      await loginHelper.logout();
+      await framePage.navigateToFindFeatPage();
+      await findFeatPage.navigateToShowPage(testFeatName);
+
+      const favourite = await showFeatPage.getFavouritesInfo();
+      expect(favourite.added).toBeNull();
+    });
+
+    it('should show favourite marks when logged in', async () => {
+      await loginHelper.login();
+      await framePage.navigateToFindFeatPage();
+      await findFeatPage.navigateToShowPage(testFeatName);
+
+      await assertFavouriteOnShowPage(false);
+    });
+
+    it('should add and remove favourite', async () => {
+      await loginHelper.login();
+      await framePage.navigateToFindFeatPage();
+      await findFeatPage.navigateToShowPage(testFeatName);
+
+      let favourite = await assertFavouriteOnShowPage(false);
+      await favourite.click();
+      await assertFavouriteOnShowPage(true);
+
+      await framePage.navigateToFavourites();
+      await favouritesPage.assertIsOnThePage();
+
+      let isFeatInFavourites = await favouritesPage.isFeatPresent(testFeatName);
+      expect(isFeatInFavourites).toBeTruthy();
+
+      await framePage.navigateToFindFeatPage();
+      await findFeatPage.navigateToShowPage(testFeatName);
+      favourite = await assertFavouriteOnShowPage(true);
+      await favourite.click();
+      favourite = await assertFavouriteOnShowPage(false);
+
+      await framePage.navigateToFavourites();
+      isFeatInFavourites = await favouritesPage.isFeatPresent(testFeatName);
+      expect(isFeatInFavourites).toBeFalsy();
     });
   });
 });

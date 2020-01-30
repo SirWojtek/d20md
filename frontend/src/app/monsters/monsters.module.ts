17e1be0d8ee9d7daa0d2bb7bd552bd1cb3d22315
Mateusz Okulewicz <mat@citrusad.com>
2020-01-30 16:32:19 +0100

    scaffoling

--- frontend/src/app/monsters/monsters.module.ts
+++ frontend/src/app/monsters/monsters.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {Title, BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
-import {monstersRouting} from './monsters.routing';
-import {MonstersPanelsModule} from './panels/monsters-panels.module';
-import {ShowMonsterComponent} from './show-monster.component';
-import {MonsterFindModule} from './find/monster-find.module';
-import {MonstersService} from './monsters.service';
-import {SharedModule} from '../shared/shared.module';
-import {AddMonsterComponent} from './add/add-monster.component';
+import { NgModule } from '@angular/core';
+import { Title, BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { monstersRouting } from './monsters.routing';
+import { MonstersPanelsModule } from './panels/monsters-panels.module';
+import { ShowMonsterComponent } from './show-monster.component';
+import { MonsterFindModule } from './find/monster-find.module';
+import { MonstersService } from './monsters.service';
+import { SharedModule } from '../shared/shared.module';
+import { AddMonsterComponent } from './add/add-monster.component';
+import { ShowAllMonsterComponent } from './show-all-monster.component';
 
 @NgModule({
   imports: [
@@ -16,9 +17,13 @@ import {AddMonsterComponent} from './add/add-monster.component';
     SharedModule,
     monstersRouting,
     MonstersPanelsModule,
-    MonsterFindModule,
+    MonsterFindModule
   ],
-  declarations: [ShowMonsterComponent, AddMonsterComponent],
-  providers: [Title, MonstersService],
+  declarations: [
+    ShowMonsterComponent,
+    ShowAllMonsterComponent,
+    AddMonsterComponent
+  ],
+  providers: [Title, MonstersService]
 })
 export class MonstersModule {}
---
+++ frontend/src/app/monsters/show-all-monster.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './show-all-monster.component.html'
+})
+export class ShowAllMonsterComponent {}

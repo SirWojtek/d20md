3044a1dbc27d2910955a09fc209bfd7cd2f53d24
Mateusz Okulewicz <mat@citrusad.com>
2020-02-11 15:16:21 +0100

    first working version

--- frontend/src/app/monsters/show-all-monster.component.ts
+++ frontend/src/app/monsters/show-all-monster.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { switchMap } from 'rxjs/operators';
+
+import { Monster } from '../shared/model/monster';
+import { MonstersService } from './monsters.service';
 
 @Component({
-  templateUrl: './show-all-monster.component.html'
+  templateUrl: './show-all-monster.component.html',
+  styleUrls: ['./show-all-monster.component.scss']
 })
-export class ShowAllMonsterComponent {}
+export class ShowAllMonsterComponent {
+  monsterOb: Observable<Monster | undefined> = this.activatedRoute.params.pipe(
+    switchMap(params => this.monstersService.getMonster(+params['id'], 'all'))
+  );
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private monstersService: MonstersService
+  ) {}
+}

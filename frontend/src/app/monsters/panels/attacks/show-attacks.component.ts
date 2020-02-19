15407fd22285e5b64cd67f32dd225b5adb531805
Mateusz Okulewicz <mat@citrusad.com>
2020-02-19 20:23:44 +0100

    view only show components

--- frontend/src/app/monsters/panels/attacks/show-attacks.component.ts
+++ frontend/src/app/monsters/panels/attacks/show-attacks.component.ts
@@ -6,30 +6,30 @@ import { Utils } from '../../../shared/utils';
 import { chromaColor } from '../../../shared/chroma-color';
 
 @Component({
-  selector : 'd20md-show-attacks',
+  selector: 'd20md-show-attacks',
   templateUrl: './show-attacks.component.html',
-  styleUrls: [ './show-attacks.component.scss' ]
+  styleUrls: ['./show-attacks.component.scss']
 })
-
 export class ShowAttacksComponent {
   @Input() attackGroups: AttackGroup[];
+  @Input() viewOnly = false;
 
   utils = Utils;
 
   private attackColor = chromaColor([-20, 0, 15, 30, 70]);
 
   public attackTypeToColor = {
-    'bludgeoning': 'burlywood',
-    'slashing': 'silver',
-    'piercing': 'olive',
-    'acid': 'limegreen',
-    'cold': 'aqua',
-    'electricity': 'yellow',
-    'fire': 'crimson',
-    'sonic': 'royalblue',
-    'force': 'hotpink',
-    'negative': 'mediumpurple',
-    'positive': 'gold',
+    bludgeoning: 'burlywood',
+    slashing: 'silver',
+    piercing: 'olive',
+    acid: 'limegreen',
+    cold: 'aqua',
+    electricity: 'yellow',
+    fire: 'crimson',
+    sonic: 'royalblue',
+    force: 'hotpink',
+    negative: 'mediumpurple',
+    positive: 'gold',
     '': 'white'
   };
 
@@ -41,7 +41,9 @@ export class ShowAttacksComponent {
     const classes = [];
 
     const index = this.attackGroups.indexOf(attackGroup);
-    if (index === -1) { return classes; }
+    if (index === -1) {
+      return classes;
+    }
 
     if (index % 2) {
       classes.push('active');
@@ -53,19 +55,42 @@ export class ShowAttacksComponent {
   public showOrRow(attackGroup: AttackGroup, attack: Attack) {
     const index = attackGroup.Attacks.indexOf(attack);
     const attackGroupLen = attackGroup.Attacks.length;
-    if (index === -1) { return false; }
+    if (index === -1) {
+      return false;
+    }
     return attackGroupLen > 1 && index !== attackGroupLen - 1;
   }
 
   public getAttackBonusInfo(attack: Attack) {
-    return attack.attack_bonus + ' + ' + attack.getAttackBonus() + ' (' + attack.getAttackAttribute() + ')';
+    return (
+      attack.attack_bonus +
+      ' + ' +
+      attack.getAttackBonus() +
+      ' (' +
+      attack.getAttackAttribute() +
+      ')'
+    );
   }
 
   public getDamageText(dmg: Damage) {
-    return dmg.dd_amount + 'k' + dmg.dd_type + ' + ' + dmg.total.damage_bonus + '/' + dmg.critical;
+    return (
+      dmg.dd_amount +
+      'k' +
+      dmg.dd_type +
+      ' + ' +
+      dmg.total.damage_bonus +
+      '/' +
+      dmg.critical
+    );
   }
 
   public getDamageInfo(dmg: Damage) {
-    return dmg.damage_bonus + ' + ' + dmg.getDamageBonus() + ' (Str) ' + dmg.description;
+    return (
+      dmg.damage_bonus +
+      ' + ' +
+      dmg.getDamageBonus() +
+      ' (Str) ' +
+      dmg.description
+    );
   }
 }

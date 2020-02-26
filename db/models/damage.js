fd27957786619f73efe8acc36b21d28bee4e241f
Mateusz Okulewicz <mat@citrusad.com>
2020-02-26 15:15:08 +0100

    other damage type

--- db/models/damage.js
+++ db/models/damage.js
@@ -1,66 +1,70 @@
-"use strict";
+'use strict';
 
 module.exports = function(sequelize, DataTypes) {
   var Damage = sequelize.define('Damage', {
-    dd_type:
-    {
+    dd_type: {
       type: DataTypes.INTEGER,
       defaultValue: 2,
       validate: {
         isIn: {
-          args: [[ 0, 2, 3, 4, 6, 8, 10, 12, 20 ]],
-          msg: 'Valid dices: 0, 2, 3, 4, 6, 8, 10, 12, 20',
-        },
-      },
+          args: [[0, 2, 3, 4, 6, 8, 10, 12, 20]],
+          msg: 'Valid dices: 0, 2, 3, 4, 6, 8, 10, 12, 20'
+        }
+      }
     },
-    dd_amount:
-    {
+    dd_amount: {
       type: DataTypes.INTEGER,
       defaultValue: 1,
       validate: {
         min: {
           args: [0],
-          msg: 'Damage dice amount cannot be lower than 0',
+          msg: 'Damage dice amount cannot be lower than 0'
         },
         max: {
           args: [100],
-          msg: 'Damage dice amount cannot be higher than 100',
-        },
-      },
+          msg: 'Damage dice amount cannot be higher than 100'
+        }
+      }
     },
-    damage_bonus:
-    {
+    damage_bonus: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
       validate: {
         min: {
           args: [-20],
-          msg: 'Damage bonus cannot be lower than -20',
+          msg: 'Damage bonus cannot be lower than -20'
         },
         max: {
           args: [50],
-          msg: 'Damage bonus cannot be higher than 50',
-        },
-      },
+          msg: 'Damage bonus cannot be higher than 50'
+        }
+      }
     },
-    damage_type:
-    {
+    damage_type: {
       type: DataTypes.ENUM,
       values: [
-        'bludgeoning', 'slashing', 'piercing',
-        'acid', 'cold', 'electricity', 'fire', 'sonic',
-        'force', 'negative', 'positive',
+        'bludgeoning',
+        'slashing',
+        'piercing',
+        'acid',
+        'cold',
+        'electricity',
+        'fire',
+        'sonic',
+        'force',
+        'negative',
+        'positive',
+        'other'
       ],
-      defaultValue: 'slashing',
+      defaultValue: 'slashing'
     },
-    critical:
-    {
+    critical: {
       type: DataTypes.STRING,
       defaultValue: 'x2',
       validate: {
         len: {
           args: [0, 20],
-          msg: 'Critical should be between 2 and 20 characters',
+          msg: 'Critical should be between 2 and 20 characters'
         }
       }
     },
@@ -69,14 +73,14 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: {
           args: [0, 50],
-          msg: 'Description cannot be longer than 50 characters',
-        },
-      },
-    },
+          msg: 'Description cannot be longer than 50 characters'
+        }
+      }
+    }
   });
 
-  Damage.afterUpdate((damage) => {
-    if(!damage.attributes.AttackId) {
+  Damage.afterUpdate(damage => {
+    if (!damage.attributes.AttackId) {
       return Damage.destroy(damage);
     }
   });

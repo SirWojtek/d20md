da7383914f1300da810fc38a1b58db5f2bef5872
Mateusz Okulewicz <mat@citrusad.com>
2020-03-07 11:24:03 +0100

    spell print page

--- frontend/src/app/spells/spells.module.ts
+++ frontend/src/app/spells/spells.module.ts
@@ -1,23 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule, Title} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { NgxPrintModule } from 'ngx-print';
 
-import {SpellsPanelsModule} from './panels/spells-panels.module';
-import {spellsRouting} from './spells.routing';
-import {ShowSpellComponent} from './show-spell.component';
-import {AddSpellComponent} from './add-spell.component';
-import {SpellsService} from './spells.service';
-import {SharedModule} from '../shared/shared.module';
-import {FindSpellsModule} from './find/find-spells.module';
+import { SpellsPanelsModule } from './panels/spells-panels.module';
+import { spellsRouting } from './spells.routing';
+import { ShowSpellComponent } from './show-spell.component';
+import { AddSpellComponent } from './add-spell.component';
+import { SpellsService } from './spells.service';
+import { SharedModule } from '../shared/shared.module';
+import { FindSpellsModule } from './find/find-spells.module';
+import { ShowAllSpellComponent } from './show-all-spell.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     SharedModule,
+    NgxPrintModule,
     spellsRouting,
     SpellsPanelsModule,
-    FindSpellsModule,
+    FindSpellsModule
   ],
-  declarations: [ShowSpellComponent, AddSpellComponent],
-  providers: [Title, SpellsService],
+  declarations: [ShowSpellComponent, ShowAllSpellComponent, AddSpellComponent],
+  providers: [Title, SpellsService]
 })
 export class SpellsModule {}

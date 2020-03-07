c71022f3ecff08f93c8997574febaf4d9fabd00b
Mateusz Okulewicz <mat@citrusad.com>
2020-03-07 11:39:46 +0100

    no image for spell details

--- frontend/src/app/spells/elements/spell-type-element.component.ts
+++ frontend/src/app/spells/elements/spell-type-element.component.ts
@@ -3,17 +3,25 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'd20md-spell-type-element',
   template: `
-  <img [src]="'assets/img/' + spellType + '.svg'">
-  <span style="font-size: 15px">{{ spellType | startCase }}</span>
+    <img
+      *ngIf="showImage; else spellTypeText"
+      [src]="'assets/img/' + spellType + '.svg'"
+    />
+    <ng-template #spellTypeText>
+      <span>Spell type: </span>
+    </ng-template>
+    <span style="font-size: 15px">{{ spellType | startCase }}</span>
   `,
   styles: [
-  `img {
-    width: 64px;
-    height: 64px;
-  }`
+    `
+      img {
+        width: 64px;
+        height: 64px;
+      }
+    `
   ]
 })
-
 export class SpellTypeElementComponent {
   @Input() spellType: string;
+  @Input() showImage = true;
 }

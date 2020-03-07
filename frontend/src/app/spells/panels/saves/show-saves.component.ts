58894132119f6e1aa31254731b6075a665ac2439
Mateusz Okulewicz <mat@citrusad.com>
2020-03-07 11:36:27 +0100

    no image for show saves

--- frontend/src/app/spells/panels/saves/show-saves.component.ts
+++ frontend/src/app/spells/panels/saves/show-saves.component.ts
@@ -3,10 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'd20md-show-saves',
   templateUrl: './show-saves.component.html',
-  styleUrls: [ './show-saves.component.scss' ]
+  styleUrls: ['./show-saves.component.scss']
 })
-
 export class ShowSavesComponent {
   @Input() save: string;
   @Input() spellResistable: boolean;
+  @Input() showImage = true;
 }
